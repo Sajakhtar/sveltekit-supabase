b08f99d5700e701775155cbde9d4feceec1212f1
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-17 23:39:43 +0400

    implement getData() function in index.svelte to display data from supabase

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import supabase from '$lib/db'
+
+  async function getData() {
+    const { data, error } = await supabase
+      .from('games')
+      .select()
+    if (error) throw new Error(error.message)
+
+    return data
+  }
+</script>
+
+<h1>My favorite games</h1>
+{#await getData()}
+  <p>Fetching data...</p>
+{:then data}
+  {#each data as game}
+    <li>{game.title}</li>
+  {/each}
+{:catch error}
+  <p>Something went wrong while fetching the data:</p>
+  <pre>{error}</pre>
+{/await}
