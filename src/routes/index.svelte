a67bdc6285d09e9d1d7996a449bca73bfcd3432a
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-18 12:09:26 +0400

    imlement send data to DB function in index.svelte and add for to send data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,9 +9,45 @@
 
     return data
   }
+
+  let newGame
+  let submit = false
+
+  async function sendData() {
+    const { data, error } = await supabase
+      .from('games')
+      .insert([
+        { 'title': newGame }
+      ]);
+
+    if (error) throw new Error(error.message)
+    return data;
+  }
+
 </script>
 
 <h1>My favorite games</h1>
+
+<!-- Send Data to DB -->
+<form on:submit|preventDefault={() => submit = true}>
+  <input type="text" bind:value={newGame}>
+  <input type="submit" value="Submit" on:click={() => submit = false}>
+</form>
+
+{#if submit}
+  {#await sendData()}
+    <p>Sending data...</p>
+  {:then data}
+    <p>Successfully sent data.</p>
+  {:catch error}
+    <p>Something went wrong while sending the data:</p>
+    <pre>{error}</pre>
+  {/await}
+{/if}
+
+
+
+<!-- Fetch Data from DB -->
 {#await getData()}
   <p>Fetching data...</p>
 {:then data}
